de83578534a1f3085d4fb825521c5a7cc3e63c95
matuskalas <matus.kalas@uib.no>
2020-07-12 15:09:49 +0000

    Copied all from fastq2fasta (ready for exercise 3)

--- src/biocpp/quality_filter.cpp
+++ src/biocpp/quality_filter.cpp
@@ -1,4 +1,15 @@
+#include <string_view>
+#include <seqan3/io/sequence_file/all.hpp>
 
-int main()
+// Alias for a C-style string.
+using character_string = char const *;
+
+// argc: number of arguments. argv[]: list of arguments. N.B. The 1st argument is the name of the program/executable.
+int main(int argc, character_string argv[])
 {
+    std::string_view fastq_file_path{argv[1]};  // Would work with (), but {} is newer & safer (memory stuff)!
+    std::string_view fasta_file_out_path{argv[2]};
+
+    seqan3::sequence_file_output{fasta_file_out_path} = // Seqan has some file extensions defined!
+        seqan3::sequence_file_input{fastq_file_path};
 }

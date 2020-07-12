1b419af37b8b6f4f816bdc72e0e91a1694856220
matuskalas <matus.kalas@uib.no>
2020-07-12 14:34:20 +0000

    Fastq2Fasta (exercise 2)

--- src/biocpp/fastq2fasta.cpp
+++ src/biocpp/fastq2fasta.cpp
@@ -1,4 +1,14 @@
+#include <string_view>
+#include <seqan3/io/sequence_file/all.hpp>
 
-int main()
+// Alias for a C-style string.
+using character_string = char const *;
+
+// argc: number of arguments. argv[]: list of arguments. N.B. The 1st argument is the name of the program/executable.
+int main(int argc, character_string argv[])
 {
+    std::string_view fastq_file_path{argv[1]};  // Would work with (), but {} is newer & safer (memory stuff)!
+
+    seqan3::sequence_file_output{std::cout, seqan3::format_fasta{}} =
+        seqan3::sequence_file_input{fastq_file_path};
 }

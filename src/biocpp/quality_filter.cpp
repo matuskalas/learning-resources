d7c5b87c99bb3b6ab500a55bc12f26360ecee7e4
matuskalas <matus.kalas@uib.no>
2020-07-12 15:11:42 +0000

    Added names for I/O files (exercise 3 intro)

--- src/biocpp/quality_filter.cpp
+++ src/biocpp/quality_filter.cpp
@@ -10,6 +10,6 @@ int main(int argc, character_string argv[])
     std::string_view fastq_file_path{argv[1]};  // Would work with (), but {} is newer & safer (memory stuff)!
     std::string_view fasta_file_out_path{argv[2]};
 
-    seqan3::sequence_file_output{fasta_file_out_path} = // Seqan has some file extensions defined!
-        seqan3::sequence_file_input{fastq_file_path};
+    seqan3::sequence_file_output fasta_out{fasta_file_out_path};
+    seqan3::sequence_file_input fastq_in{fastq_file_path};
 }
